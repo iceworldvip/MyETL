62532b9dd99de6992d19b997be48d5d11f0e2a21
iceworldvip <iceworldvip@foxmail.com>
2015-06-18 07:31:02 +0800

    初始化

---
+++ src/main/java/org/cto/etl/myetl/commons/entity/DataValue.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cto.etl.myetl.commons.entity;
+
+/**
+ * 数据值
+ *
+ * @author ice
+ */
+class DataValue {
+
+    private Object value;
+
+    /**
+     * @return the value
+     */
+    public Object getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+}
---
+++ src/main/java/org/cto/etl/myetl/commons/entity/MetaData.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cto.etl.myetl.commons.entity;
+
+/**
+ * 原数据
+ *
+ * @author ice
+ */
+public class MetaData {
+
+    /**
+     * 数据类型
+     */
+    private DataType dataType;
+
+    /**
+     * 数据值
+     */
+    private DataValue dataValue;
+
+}
